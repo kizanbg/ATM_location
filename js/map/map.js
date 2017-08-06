db4e9ffd3bc1d4aa40e05a10c2404f21c5c60a7e
kizanbg <kizanbg@gmail.com>
2017-08-06 17:42:09 +0200

    Ispis markera na mapi

--- js/map/map.js
+++ js/map/map.js
@@ -1,22 +1,35 @@
-/* Google map api key AIzaSyCBEAAeTIczihZiBFF-AnDfkJIT-yH__dg */
-
 (function(){
     "use strict";
 
+    var map,
+        currentLocation,
+        currentAtm;
+
     function showItem(index) {
-        console.log(index);
+        console.log(window.atm.atms[index]);
+
+        if(currentAtm) {
+            currentAtm.setMap(null);
+        }
+
+        currentAtm = new google.maps.Marker({
+            position: window.atm.atms[index].location,
+            map: map
+        });
     }
 
     function initMap() {
-        var uluru = {lat: window.atm.position.coords.latitude, lng: window.atm.position.coords.longitude},
-            map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 17,
-                center: uluru
-            }),
-            marker = new google.maps.Marker({
-                position: uluru,
-                map: map
-            });
+        var uluru = {lat: window.atm.position.coords.latitude, lng: window.atm.position.coords.longitude};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 17,
+            center: uluru
+        });
+
+        currentLocation = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
     }
 
     window.addEventListener("location-loaded", initMap);
